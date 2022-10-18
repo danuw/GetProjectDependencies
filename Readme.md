86c0860469ae448c4d7be3d55ae48af932e3b892
danuw <danuw@volto.io>
2022-10-18 23:25:35 +0100

    supports flat rep structure from anywhere

--- Readme.md
+++ Readme.md
@@ -1,5 +1,41 @@
+GetProjectDependencies
+======
+
+Determine the full list of all the referenced libraries for a given project (csproj)
+
+# Usage:
+
+```bash
+dotnet run <csproj full path> [<root directory to trim>]
+```
 
 
-dotnet new classlib -n lib1
+# For example:
 
-dotnet new console -n proj1
+## Example 1
+```
+dotnet run "/some/root/path/test/proj1/proj1.csproj"
+``` 
+will output 
+```
+/some/root/path/test/proj1/*
+```
+
+## Example 2:
+
+```
+dotnet run "/some/root/path/test/proj1/proj1.csproj" "/some/root/path/test/"
+```
+will output
+```
+proj1/*
+```
+
+
+
+# Other
+
+Towards creating the test project structure:
+- create console project `dotnet new console -n proj1`
+- create class lib project `dotnet new classlib -n lib1`
+- add project reference `dotnet add reference ../lib3/lib3.csproj`
--- src/Program.cs
+++ src/Program.cs
@@ -11,22 +11,29 @@ namespace cprojdepenencies
                 Console.WriteLine("dotnet run myproject.csproj");
                 return;
             }
-            var projectPath = args[0];
+            var csprojPath = args[0];
+            var projPath = "";
+            if(args.Length>1){
+                projPath = args[1];
+            }
             var listOfDependencies = new List<string>();
 
-            var listOfProjectsChecked = GetProjectDependencies(projectPath);
-            listOfProjectsChecked.Add(projectPath);
+            var listOfProjectsChecked = GetProjectDependencies(csprojPath);
+
+            listOfProjectsChecked.Add(csprojPath);
             var finalList = listOfProjectsChecked.Distinct().ToList();
-            Console.WriteLine($"'{projectPath}' contains {finalList.Count} distinct dependencies from {listOfProjectsChecked.Count} found in total");
+            Console.WriteLine($"'{csprojPath}' contains {finalList.Count} distinct dependencies from {listOfProjectsChecked.Count} found in total");
             finalList.ForEach(i => {
-                var newname = i.Replace("..\\","");
-                var lastindex = newname.LastIndexOf("\\");
-                newname = newname.Substring(0, lastindex );
-                Console.WriteLine($"     - {newname}");
+                var path = Directory.GetParent(i).FullName;
+                if(!string.IsNullOrWhiteSpace(projPath)){
+                    path = path.Replace(projPath, "");
+                }
+                Console.WriteLine($"     - {path}/*");
+                //Console.WriteLine($" - {i}");
             });
         }
 
-        static List<string> GetProjectDependencies(string projectPath)
+        static List<string> GetProjectDependencies(string csprojPath)
         {
             // if(!File.Exists(projectPath)) {
             //     Console.WriteLine($"'{projectPath}' does not exist!!");
@@ -35,13 +42,13 @@ namespace cprojdepenencies
             // Set up a set of projects using https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-add-reference
             var listOfProjectsChecked = new List<string>();
             
-            //Console.WriteLine($"Getting project dependencies for {projectPath}");
+            Console.WriteLine($"Getting project dependencies for {csprojPath}");
 
             //https://social.msdn.microsoft.com/Forums/vstudio/en-US/6df34918-8c1e-4659-b6af-5872924d36b7/how-to-open-csproj-in-xml-file-through-c-code-and-add-a-file-as-a-link?forum=csharpgeneral
          
             XmlDocument doc = new XmlDocument();
 
-            doc.Load(projectPath);
+            doc.Load(csprojPath);
 
             XmlNodeList elemList = doc.GetElementsByTagName("ProjectReference");
 
@@ -49,49 +56,80 @@ namespace cprojdepenencies
             for (int i = 0; i < elemList.Count; i++)
             {
                 string attrVal = elemList[i].Attributes["Include"].Value;
-                listOfProjectsChecked.Add(attrVal);
-                var newPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), attrVal.Replace("\\", "/")));
-                listOfProjectsChecked.AddRange(GetProjectDependencies(newPath.Canonicalize()));
+                var newcsprojPath = GetFullPath(csprojPath, attrVal);
+                
+                //listOfProjectsChecked.Add(attrVal);//record project reference as is in the file
+                listOfProjectsChecked.Add(newcsprojPath);// record full path
+                
+                listOfProjectsChecked.AddRange(GetProjectDependencies(newcsprojPath));
             }
             
             return listOfProjectsChecked;
         }
 
-
+        private static string GetFullPath(string csprojPath, string relativeCsprojPath)
+        {
+            var parentDir = "";
+            parentDir = Directory.GetParent(csprojPath).FullName;
+            System.Console.WriteLine(csprojPath);
+            System.Console.WriteLine(relativeCsprojPath);
+            if(relativeCsprojPath.Contains(".."))// we want to go to parent path
+            {
+                System.Console.WriteLine("Going up a level to ...");
+                parentDir = Directory.GetParent(parentDir).FullName;
+                System.Console.WriteLine(parentDir);
+                relativeCsprojPath = relativeCsprojPath.Replace("\\", "/");
+                relativeCsprojPath = relativeCsprojPath.Substring(3, relativeCsprojPath.Length - 3);
+            }
+            System.Console.WriteLine("We are at ...");
+                
+            System.Console.WriteLine(parentDir);
+            System.Console.WriteLine(relativeCsprojPath);
+            System.Console.WriteLine("So...");
+            System.Console.WriteLine(Path.Combine(parentDir, relativeCsprojPath));
+            
+            // System.Console.WriteLine(relativeCsprojPath);
+            // var lastindex = relativeCsprojPath.LastIndexOf("\\");
+            // System.Console.WriteLine(lastindex +"-"+relativeCsprojPath.Length);
+            // var filename = relativeCsprojPath.Substring(lastindex, relativeCsprojPath.Length - lastindex );
+            // //var filename = Path.GetFileName(relativeCsprojPath);
+            // System.Console.WriteLine(filename);
+            return Path.Combine(parentDir, relativeCsprojPath);
+        }
     }
 
-    public static class Extensions
-    {
+    // public static class Extensions
+    // {
         
-        // source: https://stackoverflow.com/a/60480478
-        /// <summary>
-        ///     Fixes "../.." etc
-        /// </summary>
-        public static string Canonicalize(this string path)
-        {
-            if (path.IsAbsolutePath())
-                return Path.GetFullPath(path);
-            var fakeRoot = Environment.CurrentDirectory; // Gives us a cross platform full path
-            var combined = Path.Combine(fakeRoot, path);
-            combined = Path.GetFullPath(combined);
-            return combined.RelativeTo(fakeRoot);
-        }
-        private static bool IsAbsolutePath(this string path)
-        {
-            if (path == null) throw new ArgumentNullException(nameof(path));
-            return
-                Path.IsPathRooted(path)
-                && !Path.GetPathRoot(path).Equals(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal)
-                && !Path.GetPathRoot(path).Equals(Path.AltDirectorySeparatorChar.ToString(), StringComparison.Ordinal);
-        }
-        private static string RelativeTo(this string filespec, string folder)
-        {
-            var pathUri = new Uri(filespec);
-            // Folders must end in a slash
-            if (!folder.EndsWith(Path.DirectorySeparatorChar.ToString())) folder += Path.DirectorySeparatorChar;
-            var folderUri = new Uri(folder);
-            return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri).ToString()
-                .Replace('/', Path.DirectorySeparatorChar));
-        }
-    }
+    //     // source: https://stackoverflow.com/a/60480478
+    //     /// <summary>
+    //     ///     Fixes "../.." etc
+    //     /// </summary>
+    //     public static string Canonicalize(this string path)
+    //     {
+    //         if (path.IsAbsolutePath())
+    //             return Path.GetFullPath(path);
+    //         var fakeRoot = Environment.CurrentDirectory; // Gives us a cross platform full path
+    //         var combined = Path.Combine(fakeRoot, path);
+    //         combined = Path.GetFullPath(combined);
+    //         return combined.RelativeTo(fakeRoot);
+    //     }
+    //     private static bool IsAbsolutePath(this string path)
+    //     {
+    //         if (path == null) throw new ArgumentNullException(nameof(path));
+    //         return
+    //             Path.IsPathRooted(path)
+    //             && !Path.GetPathRoot(path).Equals(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal)
+    //             && !Path.GetPathRoot(path).Equals(Path.AltDirectorySeparatorChar.ToString(), StringComparison.Ordinal);
+    //     }
+    //     private static string RelativeTo(this string filespec, string folder)
+    //     {
+    //         var pathUri = new Uri(filespec);
+    //         // Folders must end in a slash
+    //         if (!folder.EndsWith(Path.DirectorySeparatorChar.ToString())) folder += Path.DirectorySeparatorChar;
+    //         var folderUri = new Uri(folder);
+    //         return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri).ToString()
+    //             .Replace('/', Path.DirectorySeparatorChar));
+    //     }
+    // }
 }
